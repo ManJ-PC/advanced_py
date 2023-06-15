acc88f4b81bfaae74a9bc9b2218f8a7c7ffba285
ManJ-PC <up201202445@fe.up.pt>
2023-06-15 09:54:26 +0100

    adding files

---
+++ Aula 5 meu.py
@@ -0,0 +1,12 @@
+from Class_Aluni import*
+
+if __name__ == "__main__":
+    Inf = [["Cárine Gonçalves", 11, 13], ["José Cruz", 14, 16], ["Paulo Sousa", 18, 20], ["Gilberto Moreira", 18, 16]]
+
+    Pauta = {}
+    for a in Inf:
+        E = EstudanteInf(a[0], )
+    #E = EstudanteInf("", 0, 0)
+
+    # E.Introdução_dados()
+    # E.Apresentação
